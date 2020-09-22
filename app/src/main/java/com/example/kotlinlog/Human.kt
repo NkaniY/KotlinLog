7200ce91086c06e257e2b5b39aee44060590d3fa
NkaniY <yuuki.072395@gmail.com>
2020-09-22 18:32:54 +0900

    Thinkableインターフェースの追加(エラー)

--- app/src/main/java/com/example/kotlinlog/Human.kt
+++ app/src/main/java/com/example/kotlinlog/Human.kt
@@ -4,11 +4,19 @@ import android.util.Log
 
 open class Human :Animal{
     // 引数付きコンストラクタ
-    constructor(name: String, age: Int): super(name, age) {
+    constructor(name: String, age: Int, hobby: String){
+        this.name = name
+        this.age = age
+        this.hobby = hobby
     }
 
     // Animalクラスのメソッドをオーバーライド
     override fun say() {
         Log.d("kotlintest", "「私の名前は" + this.name + "です。年は" + this.age + "歳です。」")
+    }
+
+    // Thinkableインタフェースのメソッドをオーバーライド
+    fun think() {
+        Log.d("kotlintest", "「私は" + this.hobby + "について考える。」")
     }
 }
--- app/src/main/java/com/example/kotlinlog/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlog/MainActivity.kt
@@ -10,8 +10,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val human = Human("中野湧希", 24)      // 名前をポチ、年齢3歳で、Dogのインスタンスを作る
+        val human = Human("中野湧希", 24, "テニス")      // 名前を中野湧希、年齢24歳、趣味テニスで、Humanのインスタンスを作る
 
         human.say()
+        human.think()
     }
 }
